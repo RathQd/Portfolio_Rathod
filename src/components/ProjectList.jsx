d3e1e7b2ab90359821b76c07b70844f05d24a25f
Dharmraj Rathod <dharmrajrathod98@gmail.com>
2025-05-28 00:56:27 +0530

    project url update

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -32,9 +32,9 @@ const ProjectList = () => {
         repo= "https://github.com/RathQd/Portfolio_Rathod"/>
     <ProjectCard 
         name="Collaborative Event Management System" 
-        description = "Developed a FastAPI-based app, enabling secure event creation, sharing, versioning, and real-time updates via email, CI/CD via GitHub Actions, and Dockerized deployment." 
+        description = "Developed a FastAPI-based app, enabling secure event creation, sharing, versioning, and real-time updates via email." 
         technologies = {techProject3}
-        projectlink = "Collaborative-event-management-system-0qep.onrender.com/docs"
+        projectlink = "https://Collaborative-event-management-system-0qep.onrender.com/docs"
         repo= "https://github.com/RathQd/Collaborative-Event-Management-System"/>
     <ProjectCard 
         name="JobGlu" 
